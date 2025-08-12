92fb1c458c2c239e266e87e113fc65f3ff97d372
Maximilien Brunet <maximilien.brunet@me.com>
2025-08-12 22:27:36 +0400

    removed wrong import that prevented deploy

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import type ProjectsCarousel from "./components/ProjectsCarousel";
+
 
 const STORAGE_KEY = "lng";
 const SUPPORTED = ["en", "fr", "ru"] as const;
